a14639ad84cfe781d06bbae5fb8e67bed6c6244c
TravisAlstrand <travisalstrand@gmail.com>
2021-12-17 16:01:10 -0800

    Commit 6
    Completed project

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const phraseUL = phraseDiv.firstElementChild;
 const startBtn = document.querySelector('.btn__reset');
 const scoreDiv = document.getElementById('scoreboard');
 const scoreOL = scoreDiv.firstElementChild;
+const h2 = document.querySelector('h2');
+const hearts = document.querySelectorAll('.tries img');
+const keys = document.querySelectorAll('button');
 
 // counter for missed guesses
 let missedGuess = 0;
@@ -17,13 +20,14 @@ const phrases = [
     "hold on to your butts",
     "they left us",
     "we spared no expense",
-    "if you gotta go you gotta go"
+    "if you gotta go you gotta go",
+    "wheres the goat",
+    "nuh uh uh you didnt say the magic word"
 ];
 
-// listen for click on start button, hide welcome div, call a random phrase
+// listen for click on start button, call start function (bottom screen)
 startBtn.addEventListener('click', () => {
-        welcomeScreen.style.display = "none";
-        getRandomPhraseAsArray(phrases);
+    startAGame();
 });
 
 // get a random phrase index from phrases array
@@ -36,7 +40,7 @@ function getRandomPhraseAsArray(arr)
     addPhraseToDisplay(letterArray);
 }
 
-// createLI and display it on screen
+// createLI for each letter and display it on screen
 function addPhraseToDisplay(arr) 
 {
     arr.forEach(text => {
@@ -54,18 +58,18 @@ function addPhraseToDisplay(arr)
 }
 
 // listen for screen keyboard button click
-// if guess is wrong, increment counter & remove heart
+// if guess is wrong, increment counter & change heart
 keysDiv.addEventListener('click', (e) => {
+    
     if (e.target.tagName === "BUTTON" && e.target.className !== "chosen") 
     {
         e.target.className = "chosen";
         e.target.disabled = true;
         const matchResults = checkLetter(e.target);
-        const hearts = document.querySelectorAll('.tries img');
 
         // check if the letter clicked matches a letter in phrase
         // show letter in phrase if so
-        function checkLetter (btn) 
+        function checkLetter(btn) 
         {
             const letterLIs = phraseUL.children;
             let letterFound = null;
@@ -74,7 +78,7 @@ keysDiv.addEventListener('click', (e) => {
             {
                 if (btn.textContent === letterLIs[i].textContent) 
                 {
-                    letterLIs[i].className = "show";
+                    letterLIs[i].classList.add('show');
                     letterFound += btn.textContent;
                 }
             }
@@ -85,8 +89,67 @@ keysDiv.addEventListener('click', (e) => {
         {
             missedGuess++;
             hearts[missedGuess - 1].src = "images/lostHeart.png";
-            console.log(matchResults);
-            console.log(missedGuess);
         }
     }
+    
+    const liLetter = document.querySelectorAll('.letter');
+    let liShow = document.querySelectorAll('.show');
+
+    checkWin(liLetter.length, liShow.length);
 });
+
+function checkWin(letter, show) 
+{
+    if (show === letter && missedGuess < 5) 
+    {
+        showEndScreen("win", `You Won! <br> You're clever like a raptor!`, "win a");
+    }
+    else if (missedGuess === 5 && show !== letter)
+    {
+        showEndScreen("lose", `Oh No! <br> The T-Rex got you!`, "lose a");
+    }
+}
+
+function showEndScreen(newClass, h2Text, btnClass)
+{
+    // change overlay class and h2 text
+    welcomeScreen.className = newClass;
+    h2.innerHTML = h2Text;
+    // change button class/text
+    startBtn.className = btnClass;
+    startBtn.textContent = "Try Again?";
+    // display screen
+    welcomeScreen.style.display = "flex";
+}
+
+function startAGame()
+{
+    function fullHearts()
+    {
+        for (i = 0; i < hearts.length; i++)
+        {
+            hearts[i].src = "images/liveHeart.png";
+        }
+    }
+
+    function newKeys()
+    {
+        for (i = 0; i < keys.length; i++)
+        {
+            keys[i].classList.remove("chosen");
+            keys[i].disabled = false;
+        }
+    }
+
+    function clearPhrase()
+    {
+        phraseUL.replaceChildren();
+    }
+
+    clearPhrase();
+    newKeys();
+    fullHearts();
+    missedGuess = 0;
+    welcomeScreen.style.display = "none";
+    getRandomPhraseAsArray(phrases);
+}
